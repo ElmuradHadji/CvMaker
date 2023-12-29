4d63ebbb7457fb37cac431bd3b7de214a8cc46a3
MelekPashazada <pasazademelek314@gmail.com>
2023-12-29 20:32:45 +0400

    m_29_12_2023_commit1

--- CVMaker/Models/Education.cs
+++ CVMaker/Models/Education.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Degree { get; set; }
-        public string Univercity { get; set; }
+        public string University { get; set; }
         public string Profession { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
